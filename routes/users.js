a853477480f899b3557587fc708996b9568eb185
Ross Gruber <ross.gruber@gmail.com>
2019-12-16 21:49:17 -0800

    Go back to single route tree based on user (and file)

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 var pool = require('../db').pool
 
-console.log(pool)
 // Users
-
 //create user
 router.post('/user', (req, res) => {
   user = req.body
@@ -36,8 +34,39 @@ router.get('/users', (req, res) => {
 router.get('/user/:id', (req, res) => {
   console.log(pool)
   const userId = Number(req.params.id)
-  console.log("User_id: " + userId + " sent")
+  console.log("userId: " + userId + " sent")
   pool.query('SELECT * FROM users WHERE user_id = $1;', [userId], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+});
+
+// create friendship
+// Friends
+//create friend
+router.post('/user/:id/friend', (req, res) => {
+  friendship = req.body
+  console.log("Friendship: " + friendship.id + " " + friendship.user_id + " " + friendship.friend_id)
+  pool.query('INSERT INTO friends (id,user_id,friend_id) values ($1, $2, $3);', [friendship.id, Number(friendship.user_id), Number(friendship.friend_id)],
+   (error, results) => {
+    if (error) {
+      if (error.message.includes("duplicate key")) {
+        res.status(400).json({"msg": "overwriting of record rejected"})
+      }
+    } else {
+      res.status(200).json({"msg": "successfully inserted friend"})
+    }
+  })
+})
+
+// fetch friends of a single user
+router.get('/user/:id/friends', (req, res) => {
+  console.log(pool)
+  const userId = Number(req.params.id)
+  console.log("Friend_id: " + userId + " sent")
+  pool.query('SELECT * FROM friends WHERE user_id = $1;', [userId], (error, results) => {
     if (error) {
       throw error
     }

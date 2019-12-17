d1730791242c4aa478e4d8aa965c11738b3c18fa
Ross Gruber <ross.gruber@gmail.com>
2019-12-16 21:37:57 -0800

    Create user, get user, get users

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,12 @@ console.log(pool)
 // Users
 // fetch users
 router.get('/users', (req, res) => {
-  res.send('You should get a list of users back eventually')
+  pool.query('SELECT * FROM users;', [], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
 })
 
 // fetch single user
@@ -15,7 +20,7 @@ router.get('/user/:id', (req, res) => {
   console.log(pool)
   const userId = Number(req.params.id)
   console.log("User_id: " + userId + " sent")
-  pool.query('SELECT * FROM users WHERE id = $1', [userId], (error, results) => {
+  pool.query('SELECT * FROM users WHERE user_id = $1;', [userId], (error, results) => {
     if (error) {
       throw error
     }
@@ -25,15 +30,17 @@ router.get('/user/:id', (req, res) => {
 
 router.post('/user', (req, res) => {
   user = req.body
-
-  pool.query('INSERT INTO users VALUES ($1, $2, $3)', [user.user_id], [user.first_name], [user.last_name],
+  console.log(user.user_id + " " + user.first_name + " " + user.last_name)
+  pool.query('INSERT INTO users (user_id,first_name,last_name) values ($1, $2, $3);', [Number(user.user_id), user.first_name, user.last_name],
    (error, results) => {
     if (error) {
-      throw error
+      if (error.message.includes("duplicate key")) {
+        res.status(400).json({"msg": "overwriting of record rejected"})
+      }
+    } else {
+      res.status(200).json({"msg": "successfully inserted user"})
     }
-    res.status(200).json(results.rows)
   })
-  res.send("got it")
 })
 
 // router.get(`/users/:id`), (req, res) => {
